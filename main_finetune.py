54042a87131bfd4767a6dababd3b9029f0adca7d
bmcdanel@fandm.edu <mcdabw6@gmail.com>
2022-06-16 22:32:12 -0400

    Added magnitude-based patch sampling.

--- main_finetune.py
+++ main_finetune.py
@@ -137,6 +137,7 @@ def main(config):
         data_loader_train.sampler.set_epoch(epoch)
 
         model.module.set_patch_drop_ratio(0.5)
+        model.module.set_patch_drop_func("magnitude")
         train_one_epoch(config, model, criterion, data_loader_train, optimizer, epoch, mixup_fn, lr_scheduler)
         if dist.get_rank() == 0 and (epoch % config.SAVE_FREQ == 0 or epoch == (config.TRAIN.EPOCHS - 1)):
             save_checkpoint(config, epoch, model_without_ddp, max_accuracy, optimizer, lr_scheduler, logger)
--- models/fracpatch_vision_transformer.py
+++ models/fracpatch_vision_transformer.py
@@ -1,4 +1,5 @@
 # --------------------------------------------------------
+# Copyright (C) 2022 Brad McDannel
 # SimMIM
 # Copyright (c) 2021 Microsoft
 # Licensed under The MIT License [see LICENSE for details]
@@ -19,13 +20,14 @@ from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 def get_patch_idxs(x, drop_func, drop_ratio):
     if drop_func == "random":
         return get_random_patches(x, drop_ratio)
+    elif drop_func == "magnitude":
+        return get_magnitude_patches(x, drop_ratio)
     else:
         raise NotImplementedError
 
 def get_random_patches(x, drop_ratio):
     B, N, C = x.shape
     keep_point = int(round((1-drop_ratio)*N)) - 1
-    # keep_patches = torch.randint(1, (B, N), (keep_point,), device=x.device)
     keep_patches = torch.rand(B, N-1, device=x.device).argsort(dim=1)
     keep_patches = keep_patches[:, :keep_point]
 
@@ -37,6 +39,28 @@ def get_random_patches(x, drop_ratio):
 
     return (batch_idxs, patch_idxs), (B, keep_point + 1)
 
+def get_magnitude_patches(x, drop_ratio):
+    B, N, C = x.shape
+    keep_point = int(round((1-drop_ratio)*N)) - 1
+
+    # sample patches using their magnitude + noise
+    sample_mat = x.abs().sum(dim=2)
+    sample_mat = sample_mat / torch.sum(sample_mat, dim=1, keepdim=True)
+    noise_std = sample_mat.std()
+    noise = torch.zeros(B, N, device=x.device)
+    noise = trunc_normal_(noise, std=noise_std)
+    sample_mat = sample_mat + noise
+
+    keep_patches = sample_mat.argsort(dim=1, descending=True)
+    keep_patches = keep_patches[:, :keep_point]
+
+    # do not drop cls patch
+    cls_idx = torch.zeros((B, 1), device=keep_patches.device, dtype=keep_patches.dtype)
+
+    batch_idxs = torch.arange(B, device=x.device).repeat_interleave(keep_point + 1)
+    patch_idxs = torch.cat((cls_idx, keep_patches), dim=1).view(-1)
+
+    return (batch_idxs, patch_idxs), (B, keep_point + 1)
 
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
@@ -303,8 +327,12 @@ class FracPatchVisionTransformer(nn.Module):
         self.patch_drop_ratio = 0.0
         self.patch_drop_func = 'random'
     
+
     def set_patch_drop_ratio(self, ratio):
         self.patch_drop_ratio = ratio
+
+    def set_patch_drop_func(self, func):
+        self.patch_drop_func = func
 
     def _trunc_normal_(self, tensor, mean=0., std=1.):
         trunc_normal_(tensor, mean=mean, std=std)
